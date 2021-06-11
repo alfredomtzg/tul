44867f808d9fd8d8faaf40d60c608fd34099a1dc
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-06-11 13:57:35 -0500

    [Update] alert and error login, register

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  exports: [
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
   ]
 })
 export class MaterialModule { }
